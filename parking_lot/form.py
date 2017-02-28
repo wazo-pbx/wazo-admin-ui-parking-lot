ac696cfc56497993bf959a2991d8158ba52c28fa
Francois Blackburn <blackburnfrancois@gmail.com>
2017-02-28 13:45:43 -0500

    fix copyright

--- parking_lot/form.py
+++ parking_lot/form.py
@@ -1,4 +1,5 @@
-# -*- copyright 2017 by Sylvain Boily
+# -*- coding: utf-8 -*-
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
 from flask_wtf import FlaskForm
@@ -9,6 +10,7 @@ from wtforms.fields import IntegerField
 
 from wtforms.validators import InputRequired
 from wtforms.validators import Optional
+
 
 class ParkingLotForm(FlaskForm):
     name = TextField('Name', [InputRequired()])
