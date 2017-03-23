383ea7f4563d8c26bc38e78df326e29a803fa3b7
Francois Blackburn <blackburnfrancois@gmail.com>
2017-03-23 11:34:51 -0400

    change TextField (deprecated) to StringField

--- parking_lot/form.py
+++ parking_lot/form.py
@@ -5,7 +5,7 @@
 from flask_wtf import FlaskForm
 
 from wtforms.fields import SubmitField
-from wtforms.fields import TextField
+from wtforms.fields import StringField
 from wtforms.fields import IntegerField
 
 from wtforms.validators import InputRequired
@@ -13,10 +13,10 @@ from wtforms.validators import Optional
 
 
 class ParkingLotForm(FlaskForm):
-    name = TextField('Name', [InputRequired()])
-    extension = TextField('Extension', [InputRequired()])
-    slots_start = TextField('Slots Start', [InputRequired()])
-    slots_end = TextField('Slots End', [InputRequired()])
-    music_on_hold = TextField('Music On Hold')
+    name = StringField('Name', [InputRequired()])
+    extension = StringField('Extension', [InputRequired()])
+    slots_start = StringField('Slots Start', [InputRequired()])
+    slots_end = StringField('Slots End', [InputRequired()])
+    music_on_hold = StringField('Music On Hold')
     timeout = IntegerField('Timeout', [Optional()])
     submit = SubmitField('Submit')
