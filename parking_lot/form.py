9addea665e8ff21174eb81ff956dd8711c046ee2
Francois Blackburn <fblackburn@wazo.community>
2017-04-10 14:27:39 -0400

    remove marshmallow and use new form helper

--- parking_lot/form.py
+++ parking_lot/form.py
@@ -2,19 +2,25 @@
 # Copyright 2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
-from flask_wtf import FlaskForm
-
 from wtforms.fields import (SubmitField,
                             StringField,
                             SelectField,
+                            FieldList,
+                            FormField,
                             IntegerField)
-from wtforms.validators import InputRequired
-from wtforms.validators import Optional
+from wtforms.validators import InputRequired, Optional
+
+from wazo_admin_ui.helpers.form import BaseForm
 
 
-class ParkingLotForm(FlaskForm):
+class ExtensionForm(BaseForm):
+    exten = StringField('Extension')
+    context = StringField(default='default')
+
+
+class ParkingLotForm(BaseForm):
     name = StringField('Name', [InputRequired()])
-    extension = StringField('Extension', [InputRequired()])
+    extensions = FieldList(FormField(ExtensionForm), min_entries=1)
     slots_start = StringField('Slots Start', [InputRequired()])
     slots_end = StringField('Slots End', [InputRequired()])
     music_on_hold = SelectField('Music On Hold', choices=[])
--- parking_lot/view.py
+++ parking_lot/view.py
@@ -5,47 +5,36 @@
 from __future__ import unicode_literals
 
 from flask_menu.classy import classy_menu_item
-from marshmallow import fields
 
 from wazo_admin_ui.helpers.classful import BaseView
-from wazo_admin_ui.helpers.mallow import BaseSchema, BaseAggregatorSchema, extract_form_fields
 
 from .form import ParkingLotForm
-
-
-class ParkingLotSchema(BaseSchema):
-
-    class Meta:
-        fields = extract_form_fields(ParkingLotForm)
-
-
-class ExtensionSchema(BaseSchema):
-    context = fields.String(default='default')
-    exten = fields.String(attribute='extension')
-
-
-class AggregatorSchema(BaseAggregatorSchema):
-    _main_resource = 'parking_lot'
-
-    parking_lot = fields.Nested(ParkingLotSchema)
-    extension = fields.Nested(ExtensionSchema)
 
 
 class ParkingLotView(BaseView):
 
     form = ParkingLotForm
     resource = 'parking_lots'
-    schema = AggregatorSchema
 
     @classy_menu_item('.parkinglots', 'Parking Lots', order=2, icon="automobile")
     def index(self):
         return super(ParkingLotView, self).index()
 
     def _map_resources_to_form(self, resources):
-        schema = self.schema()
-        data = schema.load(resources).data
-        main_exten = schema.get_main_exten(resources['parking_lot'].get('extensions', {}))
-        form = self.form(data=data['parking_lot'], extension=main_exten)
+        form = self.form(data=resources['parking_lot'])
         moh_name = resources['parking_lot'].get('music_on_hold')
         form.music_on_hold.choices = [(moh_name, moh_name)]
+        return form
+
+    def _map_form_to_resources(self, form, form_id=None):
+        parking_lot = form.to_dict()
+        resources = {'parking_lot': parking_lot,
+                     'extension': parking_lot['extensions'][0]}
+        if form_id:
+            resources['parking_lot']['id'] = form_id
+        return resources
+
+    def _map_resources_to_form_errors(self, form, resources):
+        form.populate_errors(resources.get('parking_lot', {}))
+        form.extensions[0].populate_errors(resources.get('extension', {}))
         return form
