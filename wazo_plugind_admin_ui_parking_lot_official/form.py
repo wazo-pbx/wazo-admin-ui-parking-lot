62fc68536ca9e7db606adf49d726ab4d534129a2
Cedric Abunar <cabunar@wazo.io>
2018-01-11 13:15:12 -0500

    update code to use translations

--- wazo_plugind_admin_ui_parking_lot_official/form.py
+++ wazo_plugind_admin_ui_parking_lot_official/form.py
@@ -1,6 +1,7 @@
 # Copyright 2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
+from flask_babel import lazy_gettext as l_
 from wtforms.fields import (SubmitField,
                             StringField,
                             SelectField,
@@ -13,15 +14,15 @@ from wazo_admin_ui.helpers.form import BaseForm
 
 
 class ExtensionForm(BaseForm):
-    exten = StringField('Extension', validators=[InputRequired()])
-    context = SelectField('Context', validators=[InputRequired()])
+    exten = StringField(l_('Extension'), validators=[InputRequired()])
+    context = SelectField(l_('Context'), validators=[InputRequired()])
 
 
 class ParkingLotForm(BaseForm):
-    name = StringField('Name', [InputRequired(), Length(max=128)])
+    name = StringField(l_('Name'), [InputRequired(), Length(max=128)])
     extensions = FieldList(FormField(ExtensionForm), min_entries=1)
-    slots_start = StringField('Slots Start', [InputRequired(), Regexp(r'^[0-9]+$'), Length(max=40)])
-    slots_end = StringField('Slots End', [InputRequired(), Regexp(r'^[0-9]+$'), Length(max=40)])
-    music_on_hold = SelectField('Music On Hold', [Length(max=128)], choices=[])
-    timeout = IntegerField('Timeout', [NumberRange(min=0)])
-    submit = SubmitField('Submit')
+    slots_start = StringField(l_('Slots Start'), [InputRequired(), Regexp(r'^[0-9]+$'), Length(max=40)])
+    slots_end = StringField(l_('Slots End'), [InputRequired(), Regexp(r'^[0-9]+$'), Length(max=40)])
+    music_on_hold = SelectField(l_('Music On Hold'), [Length(max=128)], choices=[])
+    timeout = IntegerField(l_('Timeout'), [NumberRange(min=0)])
+    submit = SubmitField(l_('Submit'))
